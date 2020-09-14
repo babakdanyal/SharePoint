cb8b82906e7800a913790c104a3b6c6e87fa5572
Babak Danyal <babakdanyal@live.com>
2020-09-14 20:26:34 +1000

    Create EditNewsFirstPublishedDate.ps1
    script to update the original published date of news articles in the SharePoint site.

---
+++ Scripts/EditNewsFirstPublishedDate.ps1
@@ -0,0 +1,19 @@
+#start region
+#
+#Use this script to update the original published date of news articles in the SharePoint site.
+#
+#end region
+$SiteURL = "<site url goes here>"
+$ListName= "Site Pages"
+$Created = "2019-12-20"
+$NewsID = 10
+Connect-PnPOnline -Url $SiteURL -UseWebLogin
+
+if (-not (Get-PnPContext)) {
+    Write-Host "Error connecting to SharePoint Online, unable to establish context" -foregroundcolor black -backgroundcolor Red
+    return
+}
+else{
+    Set-PnPListItem -List $ListName -Identity $NewsID -Values @{"FirstPublishedDate"=$Created;"Created"=$Created;"Modified"=$Created;} -SystemUpdate:$true
+}
+    
